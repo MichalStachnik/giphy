ebf8abf1aa9ce949eb769004388abbbd3bfb34c0
Michal <Michal.stachnik@my.liu.edu>
2018-05-20 23:42:38 -0400

    added click handler and transition on images

--- src/App.js
+++ src/App.js
@@ -2,7 +2,6 @@ import React, { Component } from 'react';
 import './App.css';
 import Search from './components/Search';
 
-
 class App extends Component {
   constructor(props){
     super(props);
@@ -10,17 +9,19 @@ class App extends Component {
       searchVal: ''
     }
   }
-
-  // onInputChange(e){
-  //   console.log(e.target.value);
-  // }
   onInputChange = (e) => {
     this.setState({ searchVal: e.target.value });
+  }
+  onClickClear = () => {
+    this.setState({ searchVal: '' });
   }
   render() {
     return (
       <div>
-        <input onKeyUp={this.onInputChange}/>
+        <div className="container">
+          <input placeholder="Search" value={this.state.searchVal} onChange={this.onInputChange}/>
+          <div className="clear" onClick={() => this.onClickClear()}>clear</div>
+        </div>
         <Search value={this.state.searchVal}/>
       </div>
     );
--- src/components/Search.js
+++ src/components/Search.js
@@ -6,40 +6,62 @@ class Search extends Component {
     this.state = {
       imgArray: [],
       searchVal: 'funny cat',
-      selectedImg: null
+      selectedImg: null,
+      opacity: 0
     }
   }
-  //one behind in search right now
   componentWillReceiveProps(newProps){
-    this.setState({searchVal: newProps.value})
-    let url = `http://api.giphy.com/v1/gifs/search?q=${this.state.searchVal}&api_key=TvKQCH3GjnVigxzQ8GipgTTdrXDk6KJS`;
-    fetch(url)
-      .then(res => res.json())
-      .then(myData => this.setState({
-        imgArray: myData.data.splice(0,8)
-      }, () => console.log('from componentWillReceiveProps',this.state)));
-      
+    this.setState({searchVal: newProps.value}, () => {
+      let url = `http://api.giphy.com/v1/gifs/search?q=${this.state.searchVal}&api_key=TvKQCH3GjnVigxzQ8GipgTTdrXDk6KJS`;
+      fetch(url)
+        .then(res => res.json())
+        .then(myData => this.setState({
+          imgArray: myData.data.splice(0,8)
+        }));
+    });
   }
   handleImgClick = (img) => {
-    console.log(img);
-    this.setState({selectedImg: img});
-    console.log(this.state);
+    this.setState({
+      selectedImg: img,
+      opacity: 1,
+      zIndex: 5
+    });
   }
-
+  unselectImg = () => {
+    this.setState({
+      opacity: 0,
+      zIndex: -5
+    });
+  }
   render() {
-    let selectedImage = this.state.selectedImg;
-    
     if(this.state.imgArray.length > 0){
       return(
         <div>
-          {selectedImage ?  <img src={selectedImage.images.fixed_height.url} /> : ''}
-          {console.log('this.state',this.state)}
+        <div 
+          className="modalContainer"
+          style={{ opacity: this.state.opacity, zIndex: this.state.zIndex }}
+        >
+          {this.state.selectedImg ?  
+            <img
+              className="modalImg"
+              onClick={() => this.unselectImg()} 
+              alt="" 
+              src={this.state.selectedImg.images.fixed_height.url} 
+            /> 
+            : ''}
+          </div>
           <div className="gifs">
             <ul>
               {this.state.imgArray.map((image, index) => {
                 return (
                   <li key={image.id} className={`gif${index}`}>
-                    <img alt="" onClick={() => this.handleImgClick(image)} src={image.images.downsized.url}/>
+                    <img 
+                      alt="" 
+                      onClick={() => this.handleImgClick(image)} 
+                      src={image.images.downsized.url}
+                    />
+                    <p><em>Uploaded at: </em></p>
+                    <p>{image.import_datetime}</p>
                   </li>
                 );
               })}
@@ -51,7 +73,7 @@ class Search extends Component {
     //waiting for http request
     else{
       return(
-        <p>waiting</p>
+        <p className="mt-15">no results</p>
       );
     }
   }
